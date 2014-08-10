a223a12ff0d5a4c4a0a4d9b9848784963ea32317
Lee <jessy1092@gmail.com>
2014-08-11 00:57:28 +0800

    test batch file

--- deploy.bat
+++ deploy.bat
@@ -1,18 +1,25 @@
-for %%X in (git.exe) do (set FOUND=%%~$PATH:X)
-if not defined FOUND goto NOGIT
 
-git checkout gh-pages
-git merge master
-call jade . -o . --pretty
-git add .
-git commit -m 'deploy'
-git pull
-git push
-git checkout master
-pause
-exit 0
+git --version
 
-:NOGIT
-echo Can't find Git!
-pause
-exit 1
+set upstream=lee
+set REPO=git@github.com:jessy1092/hackfoldr-2.0
+
+git fetch %upstream%
+
+rm -rf _public
+
+rem XXX: use --reference when not in shallow clone
+rem git clone $REPO --reference . -b gh-pages _public
+git clone %REPO% -b gh-pages _public
+
+set REV=`git describe --always`
+
+call ./node_modules/.bin/gulp --require LiveScript build
+
+cd _public
+
+git add -A .
+ECHO "regen for %REV%" | git commit-tree `git write-tree` -p `git rev-parse HEAD` -p %REV% | xargs git reset --hard
+git push origin gh-pages
+
+cd ..
